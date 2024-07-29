0989fd03b9e7837b94b02aff943c9522d6ae9922
Thom Rawson <t.rawson@imperial.ac.uk>
2024-07-29 16:15:32 +0100

    Function to load the names of all US states. Used as default for fitting tasks.

--- R/inputs.R
+++ R/inputs.R
@@ -52,3 +52,14 @@ cowflu_fixed_inputs <- function(n_herds_per_region, p_region_export,
        time_test = time_test,
        n_test = n_test)
 }
+
+continental_US_regions <- function(){
+  states <- c("Alabama", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+              "Florida", "Georgia", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+              "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
+              "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
+              "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
+              "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+              "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming")
+  return(states)
+}
