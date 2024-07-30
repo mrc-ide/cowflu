a71c178180393dc45f10b21c8de77d982576f5a1
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-07-30 15:39:11 +0100

    Use custom herd size

--- tests/testthat/helper-cowflu.R
+++ tests/testthat/helper-cowflu.R
@@ -1,9 +1,7 @@
 test_fixed_inputs <- function() {
   inputs <- readRDS("inputs.rds")
-  cowflu_fixed_inputs(inputs$data$n_herds_per_region,
-                      inputs$data$p_region_export,
+  cowflu_fixed_inputs(inputs$data$p_region_export,
                       inputs$data$p_cow_export,
-                      inputs$data$mean_herd_size,
                       inputs$movement_matrix,
                       which(inputs$data$name == "Texas"))
 }
