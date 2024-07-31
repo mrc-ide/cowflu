825b14526798fa1160ece0153d2352da8dfc3ad2
Thom Rawson <t.rawson@imperial.ac.uk>
2024-07-31 23:37:47 +0100

    Include usda_data in movement function call.

--- raw_data/process.R
+++ raw_data/process.R
@@ -3,7 +3,7 @@ source(file.path(root, "raw_data", "movement.R"))
 source(file.path(root, "raw_data", "herd_sizes.R"))
 
 usda_data <- real_dairy_populations(root)
-movement <- process_movement(root)
+movement <- process_movement(root, usda_data)
 
 save(list = c("usda_data", "movement"),
      file = file.path(root, "R/sysdata.rda"),
