74ce5166dbcbfc9eb7a540761e56ac5d9db7ef57
Thom Rawson <t.rawson@imperial.ac.uk>
2024-08-19 13:25:30 +0100

    This is a test written to catch this movement_matrix bug. It checks that a toy example with an identity movement_matrix has region totals always stay the same.

--- tests/testthat/helper-cowflu.R
+++ tests/testthat/helper-cowflu.R
@@ -1,6 +1,7 @@
 ## Simple toy example, symmetric migration with three regions
 test_toy_inputs <- function(alpha = 0.2, beta = 0.9, gamma = 0.1,
-                            sigma = 0.125, start_count = 5) {
+                            sigma = 0.125, start_count = 5,
+                            movement_matrix = cbind(c(.6, .2, .2), c(.2, .6, .2), c(.2, .2, .6))) {
   cowflu_inputs(
     alpha = alpha,
     beta = beta,
@@ -11,7 +12,7 @@ test_toy_inputs <- function(alpha = 0.2, beta = 0.9, gamma = 0.1,
       p_region_export = c(.5, .5, .5),
       p_cow_export = c(0.2, 0.2, 0.2),
       n_cows_per_herd = c(rep(200, 3), rep(1000, 7), rep(3000, 11)),
-      movement_matrix = cbind(c(.6, .2, .2), c(.2, .6, .2), c(.2, .2, .6)),
+      movement_matrix = movement_matrix,
       time_test = 10000,
       start_herd = 4,
       start_count = start_count))
---
+++ tests/testthat/test-movement_matrix.R
@@ -0,0 +1,27 @@
+test_that("Identity movement matrix is observed", {
+  pars <- test_toy_inputs(movement_matrix = cbind(c(1, 0, 0), c(0, 1, 0), c(0, 0, 1)))
+  n_particles <- 3
+  times <- 0:75
+  sys <- dust2::dust_system_create(cows(), pars, n_particles = n_particles, dt = 0.25)
+  dust2::dust_system_set_state_initial(sys)
+  s0 <- array(dust2::dust_system_state(sys),
+              c(pars$n_herds + pars$n_regions, 4))
+  s <- dust2::dust_system_simulate(sys, times)
+  s1 <- array(s, c(pars$n_herds + pars$n_regions, 4, n_particles, length(times)))
+
+  s1_total <- s1[22:24, , , ]
+  s1_region_totals <- apply(s1_total, c(1, 3, 4), sum)
+
+  ## Define the dimensions of s1_region_totals
+  dims <- dim(s1_region_totals)
+  ## Loop over the first (region) and second (particle) dimensions
+  for (x in 1:dims[1]) {
+    for (y in 1:dims[2]) {
+      # Extract the vector for the current (x, y)
+      slice_vector <- s1_region_totals[x, y, ]
+      # Check if all elements in the slice_vector are identical
+      expect_true(length(unique(slice_vector)) == 1,
+                  info = sprintf("Values in slice s1_region_totals[%d, %d, ] are not identical", x, y))
+    }
+  }
+})
