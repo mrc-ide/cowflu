aee2c306a3c0e5998af622a12fa2e1801cb7270b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-08-23 15:23:15 +0100

    Remove day as the prime measure of time

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 process_data <- function(data) {
   states <- tolower(usda_data$US_States)
   start_date <- as.Date("2023-12-18")
-  data$time <- as.numeric(data$Week_Beginning - start_date)
+  data$day <- as.numeric(data$Week_Beginning - start_date)
   data$State <- tolower(data$State)
   d <- unname(split(data, data$Week_Beginning))
   positive_tests <- lapply(d, function(x) {
@@ -9,6 +9,8 @@ process_data <- function(data) {
     stopifnot(!any(is.na(i)))
     x$Positive_Tests[i]
   })
-  time <- vapply(d, function(x) x$time[[1]], numeric(1))
-  data.frame(time = time, positive_tests = I(positive_tests))
+  day <- vapply(d, function(x) x$day[[1]], numeric(1))
+  data.frame(day = day,
+             week = day / 7,
+             positive_tests = I(positive_tests))
 }
